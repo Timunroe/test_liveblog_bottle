98c5ea654fbb3205d86a00c7c8274967bb52e743
Tim Munroe <munroe.tim@gmail.com>
2018-11-14 18:33:39 -0500

    Nov14 early

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from bottle import Bottle, run, template
+from bottle import Bottle, run, template, request, static_file
 import db
 
 app = Bottle()
+
+
+@app.route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
 
 
 @app.route('/')
@@ -10,10 +15,27 @@ def index():
     return template('index.html', data=db.get_pages())
 
 
-@app.route('/page/<page>')
-def posts(page):
+@app.route('/page/<page_id>')
+def posts(page_id):
     # get list of posts for this page
-    return template('page.html', data=db.get_posts(page))
+    return template('page.html', data=db.get_posts(page_id))
+
+
+@app.route('/edit/<page_id>/<post_id>')
+def edit(page_id, post_id):
+    # get post specified
+    # post = db.get_posts(page_id, post_id)
+    # author = db.get_author(post['author'])
+    return template('edit.html', data=db.get_posts(page_id, post_id), authors=db.get_authors(), ref_page=page_id)
+
+
+@app.route('/save/<page_id>/<post_id>', method='POST')
+def save(page_id, post_id):
+    # get list of posts for this page
+    print("Page id: ", page_id)
+    print("Post id: ", post_id)
+    content = request.forms.get('content')
+    print("Content: ", content.replace('\r\n', ''))
 
 
 run(app, host='localhost', port=8080, debug=True)
--- db.py
+++ db.py
@@ -1,24 +1,32 @@
-data = {
+from operator import itemgetter
+
+db = {
+    "authors": [
+        {"name": "Steve Milton", "lname": "Milton", "twitter": "miltonatthespec", "email": "smilton@thespec.com", "img": "https://dynamicmedia.zuza.com/zz/m/cropped_140x210/6/9/693a770a-7a2e-43eb-92ba-8b51acdafc50/smilton-authorbio.jpg"},
+        {"name": "Matthew Van dongen", "lname": "Van Dongen", "twitter": "mattatthespec", "email": "mvandongen@thespec.com", "img": "https://dynamicmedia.zuza.com/zz/m/cropped_140x210/6/7/67596cc1-f2d3-41ea-9a16-eae81fc682b3/mvandongen-authorbio.jpg"},
+        {"name": "Nicole O'Reilly", "lname": "O'Reilly", "twitter": "nicoleatthespec", "email": "noreilly@thespec.com", "img": "https://dynamicmedia.zuza.com/zz/m/cropped_140x210/a/0/a070cded-9302-4649-95df-1e5ddf819f3e/noreilly-authorbio.jpg"},
+        {"name": "Natalie Paddon", "lname": "Paddon", "twitter": "natatthespec", "email": "npaddon@thespec.com", "img": "https://dynamicmedia.zuza.com/zz/m/cropped_140x210/b/c/bc25c4b8-1ca6-4098-87bd-c0b59e504738/natalie.jpg"},
+    ],
     "pages": [
         {
-            "title": "weather nov 9", "slug": "weather-nov-9", "timestamp": "11-09-2018", "posts": [
-                {"headline": "Warning ended", "slug": "random5", "timestamp": "11-09-2018", "author": "Dave Phillips", "content": "Dolor id amet duis labore duis laboris fugiat veniam. Duis cillum est ad deserunt excepteur minim non sint eu nisi exercitation duis ex. Anim culpa elit excepteur Lorem aliqua voluptate consequat est. Laborum laborum elit est duis nisi quis laborum sunt ad sint ea eiusmod dolor reprehenderit. Labore reprehenderit veniam deserunt pariatur dolore Lorem voluptate exercitation do tempor."},
-                {"headline": "", "slug": "random8", "timestamp": "11-09-2018", "author": "Jim Brown", "content": "Do eu dolor excepteur officia aliqua consectetur excepteur. Velit duis enim veniam laborum quis. Aliqua consequat fugiat aute aliquip ad adipisicing culpa occaecat do quis ex. Ex consequat excepteur laborum dolor dolor reprehenderit adipisicing velit consectetur eiusmod. Veniam culpa mollit aliquip laboris reprehenderit. Id consequat ad amet ad irure officia aute veniam sit veniam aute mollit."},
-                {"headline": "Heavy rain warning", "slug": "heavy-rain-warning", "timestamp": "11-09-2018", "author": "Dave Phillips", "content": "Id consectetur proident fugiat consequat sint enim exercitation dolore. Nulla voluptate reprehenderit aute ea minim adipisicing. Fugiat nulla cupidatat incididunt laborum tempor ipsum. Non magna veniam eu nulla ipsum quis ea quis occaecat laborum cillum. Nisi qui nisi aute ea amet ullamco esse aliqua. Eu officia anim minim dolore incididunt cillum enim. Amet anim tempor aliqua esse elit tempor nostrud."},
+            "title": "weather nov 9", "page_id": "1", "slug": "weather-nov-9", "timestamp": "11-09-2018", "DNN_asset": "", "posts": [
+                {"headline": "Warning ended", "status": "live", "post_id": "1", "timestamp": "11-09-2018", "author": "Natalie Paddon", "content": "<p>Chambray <b>hoodie</b> cornhole, <em>neutra DIY</em> narwhal kombucha. &#8220;Bob&#8221;, he said. &#8220;Don&#8217;t go.&#8221; Yuccie squid literally drinking vinegar aesthetic typewriter. Normcore poke microdosing chillwave VHS godard. Aesthetic artisan chartreuse affogato plaid squid bespoke DIY.</p>"},
+                {"headline": "", "status": "draft", "post_id": "2", "timestamp": "11-09-2018", "author": "Natalie Paddon", "content": "<p>Man braid next level coloring book tumblr waistcoat air plant. Twee XOXO intelligentsia lyft, butcher air plant taiyaki kitsch echo park trust fund. Brooklyn hell of keytar cred gastropub raclette. Hell of pug beard, copper mug fam disrupt kickstarter four dollar toast. Four dollar toast lo-fi single-origin coffee plaid af selfies messenger bag cardigan typewriter authentic.</p>"},
+                {"headline": "Heavy rain warning", "status": "live", "post_id": "3", "timestamp": "11-09-2018", "author": "Nicole O'Reilly", "content": "<p>Air plant celiac snackwave lumbersexual jianbing. Pour-over letterpress taxidermy pickled prism tilde. Franzen meh VHS enamel pin pork belly. Yr tumeric snackwave, squid vinyl hammock prism cray swag truffaut scenester food truck vexillologist.</p>"},
             ]
         },
         {
-            "title": "council nov 4", "slug": "council-nov-4", "timestamp": "11-04-2018", "posts": [
-                {"headline": "Council votes 15-1", "slug": "random3", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "Esse amet minim dolor aliquip. Adipisicing incididunt ex officia labore. Nulla consectetur non Lorem magna in nulla. Quis velit sint incididunt id elit cupidatat exercitation eu est. Ea sint veniam eiusmod quis veniam consequat."},
-                {"headline": "", "slug": "random1", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "Reprehenderit eiusmod id dolor qui amet tempor enim ullamco ea. Dolor tempor adipisicing deserunt cupidatat. Amet qui laboris cillum et nisi eiusmod deserunt voluptate adipisicing. Et labore veniam cillum non id cupidatat exercitation consequat deserunt laborum. Minim eiusmod quis consectetur magna occaecat voluptate veniam laborum sunt tempor amet. Excepteur incididunt laboris minim labore nulla culpa nulla."},
-                {"headline": "Report recommends demolition", "slug": "report-recommends-demolition", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "Ullamco do minim cupidatat id id esse nulla ullamco enim excepteur est nulla. Non est occaecat tempor nulla laborum ad nisi labore excepteur id cupidatat cupidatat amet enim. Proident enim mollit irure officia sit. Qui amet commodo eu non non Lorem occaecat et. Ea enim sit nostrud dolore. Esse velit dolor elit enim ipsum ut ea fugiat."},
+            "title": "council nov 4", "page_id": "2", "slug": "council-nov-4", "timestamp": "11-04-2018", "DNN_asset": "", "posts": [
+                {"headline": "Council votes 15-1", "status": "live", "post_id": "1", "timestamp": "11-09-2018", "author": "Matthew Van Dongen", "content": "<p>Esse amet minim dolor aliquip. Adipisicing incididunt ex officia labore. Nulla consectetur non Lorem magna in nulla. Quis velit sint incididunt id elit cupidatat exercitation eu est. Ea sint veniam eiusmod quis veniam consequat.</p>"},
+                {"headline": "", "status": "live", "post_id": "2", "timestamp": "11-09-2018", "author": "Matthew Van Dongen", "content": "<p>Reprehenderit eiusmod id dolor qui amet tempor enim ullamco ea. Dolor tempor adipisicing deserunt cupidatat. Amet qui laboris cillum et nisi eiusmod deserunt voluptate adipisicing. Et labore veniam cillum non id cupidatat exercitation consequat deserunt laborum. Minim eiusmod quis consectetur magna occaecat voluptate veniam laborum sunt tempor amet. Excepteur incididunt laboris minim labore nulla culpa nulla.</p>"},
+                {"headline": "Report recommends demolition", "status": "live", "post_id": "3", "timestamp": "11-09-2018", "author": "Matthew Van Dongen", "content": "<p>Ullamco do minim cupidatat id id esse nulla ullamco enim excepteur est nulla. Non est occaecat tempor nulla laborum ad nisi labore excepteur id cupidatat cupidatat amet enim. Proident enim mollit irure officia sit. Qui amet commodo eu non non Lorem occaecat et. Ea enim sit nostrud dolore. Esse velit dolor elit enim ipsum ut ea fugiat.</p>"},
             ]
         },
         {
-            "title": "Breaking news nov 6", "slug": "breaking-news-nov-6", "timestamp": "11-06-2018", "posts": [
-                {"headline": "Council votes 15-1", "slug": "council-votes-15-1", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "Id veniam ipsum aliquip excepteur sunt est. Pariatur ad nostrud et reprehenderit fugiat aliqua Lorem ad et. Eiusmod laboris est aliqua aliquip qui aute. Elit deserunt mollit officia veniam sint culpa cupidatat."},
-                {"headline": "", "slug": "random2", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "Elit aliquip ullamco deserunt ut qui deserunt ad occaecat sunt elit. Culpa elit fugiat consectetur est. Magna magna adipisicing et ut aliqua laboris ea duis eiusmod."},
-                {"headline": "Report recommends demolition", "slug": "report-recommends-demolition", "timestamp": "11-09-2018", "author": "Sydney Green", "content": "In aliquip excepteur est nisi duis ea. Exercitation ad pariatur cillum minim non qui in velit aliquip. Ad minim nisi dolor irure. Ad ex exercitation magna ut laboris ullamco qui est in fugiat non cillum dolore pariatur. Ad reprehenderit duis aute ipsum labore tempor aliquip incididunt aliquip aute cupidatat."},
+            "title": "Breaking news nov 6", "page_id": "3", "slug": "breaking-news-nov-6", "timestamp": "11-06-2018", "DNN_asset": "", "posts": [
+                {"headline": "Council votes 15-1", "status": "live", "post_id": "1", "timestamp": "11-09-2018", "author": "Steve Milton", "content": "Id veniam ipsum aliquip excepteur sunt est. Pariatur ad nostrud et reprehenderit fugiat aliqua Lorem ad et. Eiusmod laboris est aliqua aliquip qui aute. Elit deserunt mollit officia veniam sint culpa cupidatat."},
+                {"headline": "", "status": "live", "post_id": "2", "timestamp": "11-09-2018", "author": "Nicole O'Reilly", "content": "Elit aliquip ullamco deserunt ut qui deserunt ad occaecat sunt elit. Culpa elit fugiat consectetur est. Magna magna adipisicing et ut aliqua laboris ea duis eiusmod."},
+                {"headline": "Report recommends demolition", "status": "live", "post_id": "3", "timestamp": "11-09-2018", "author": "Natalie Paddon", "content": "In aliquip excepteur est nisi duis ea. Exercitation ad pariatur cillum minim non qui in velit aliquip. Ad minim nisi dolor irure. Ad ex exercitation magna ut laboris ullamco qui est in fugiat non cillum dolore pariatur. Ad reprehenderit duis aute ipsum labore tempor aliquip incididunt aliquip aute cupidatat."},
             ]
         },
     ]
@@ -26,11 +34,26 @@ data = {
 
 
 def get_pages():
-    pages_list = [(item["title"], item["slug"]) for item in data["pages"]]
+    pages_list = [(item["title"], item["page_id"]) for item in db["pages"]]
     return pages_list
 
 
-def get_posts(page):
-    posts_list = next((item for item in data["pages"] if item["slug"] == page))
-    print(posts_list)
-    return posts_list
+def get_posts(page_id, post_id=None):
+    page = next((item for item in db["pages"] if item["page_id"] == page_id))
+    # print(page)
+    if post_id:
+        post = next((item for item in page['posts'] if item["post_id"] == post_id))
+        return post
+    else:
+        # print(posts_list)
+        return page
+
+
+def get_author(name):
+    author = next((item for item in db["authors"] if item["name"].lowercase() == name.lowercase()))
+    return author
+
+
+def get_authors():
+    authors = sorted(db['authors'], key=itemgetter('lname'))
+    return authors
