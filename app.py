f9b428643857c242629ca2d6b9bcc9cc1ea2aa08
Tim Munroe <munroe.tim@gmail.com>
2018-11-19 08:56:07 -0500

    Nov18

--- app.py
+++ app.py
@@ -12,30 +12,42 @@ def server_static(filename):
 @app.route('/')
 def index():
     # get list of pages
-    return template('index.html', data=db.get_pages())
+    return template('pages.html', data=db.get_pages())
 
 
 @app.route('/page/<page_id>')
 def posts(page_id):
+    # if id is 'new', then create page
+    if page_id == 'new':
+
+    # else get list of posts for this page
+    else:
+        return template('page.html', data=db.get_posts(page_id))
+
+
+@app.route('/edit/page/<page_id>')
+@app.route('/edit/page/<page_id>/post/<post_id>')
+def edit(page_id, post_id=None):
+    # dealing with editing a new page or existing page
+    if post_id is None:
+        return template('page_edit.html', data=db.get_posts(page_id, post_id))
+    else:
+        # dealing with editing or creating a new post belonging to a page
+        return template('post_edit.html', data=db.get_posts(page_id, post_id), authors=db.get_authors())
+
+@app.route('/save/page/<page_id>', method='POST')
+@app.route('/save/page/<page_id>/post/<post_id>', method='POST')
+def save(page_id, post_id=None):
     # get list of posts for this page
-    return template('page.html', data=db.get_posts(page_id))
+    if post_id:
+        print("Page id: ", page_id)
+        print("Post id: ", post_id)
+        content = request.forms.get('content')
+        print("Content: ", content.replace('\r\n', ''))
+    else:
+        print("Page id: ", page_id)
+        title = request.forms.get('title')
+        print("Title: ", title)
 
 
-@app.route('/edit/<page_id>/<post_id>')
-def edit(page_id, post_id):
-    # get post specified
-    # post = db.get_posts(page_id, post_id)
-    # author = db.get_author(post['author'])
-    return template('edit.html', data=db.get_posts(page_id, post_id), authors=db.get_authors(), ref_page=page_id)
-
-
-@app.route('/save/<page_id>/<post_id>', method='POST')
-def save(page_id, post_id):
-    # get list of posts for this page
-    print("Page id: ", page_id)
-    print("Post id: ", post_id)
-    content = request.forms.get('content')
-    print("Content: ", content.replace('\r\n', ''))
-
-
-run(app, host='localhost', port=8080, debug=True)
+run(app, host='localhost', port=8080, debug=True, reloader=True)
