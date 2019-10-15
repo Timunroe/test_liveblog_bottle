87e728420c063eb62bedb07c0144217fe586951d
Tim Munroe <munroe.tim@gmail.com>
2019-10-15 10:24:01 -0400

    Oct14 early

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 from bottle import Bottle, run, template, request, static_file
+from tinydb import TinyDB, Query
 import db
 
 HOST = '0.0.0.0'
 PORT = 5002
 
 app = Bottle()
+
+'''
+Need app to allow user to create a stream of a blog page
+-- a stream of micro-posts, 
+either text/HTML, or Twitter/Instagram/Youtube embed ---
+and publish as a .json file to S3,
+so a javascript script linked to a DNN article will pull it in,
+parse and add the HTML to certain divs.
++++
+NEED TABLE FOR EACH 'blog/page'.
+'''
+
+non_url_safe = ['"', '#', '$', '%', '&', '+',
+                ',', '/', ':', ';', '=', '?',
+                '@', '[', '\\', ']', '^', '`',
+                '{', '|', '}', '~', "'"]
+translate_table = {ord(char): u'' for char in non_url_safe}
+
+def slugify(text):
+    text = text.translate(translate_table)
+    text = u'_'.join(text.split())
+    return text
+
+sites = ['spectator', 'record', 'niagara', 'examiner']
+default = 'spectator'
+for site in sites:
+    TinyDB(f'''./blogs/{site}.json''')
+
+# db = TinyDB(f'''./blogs/{default}.json''')
 
 
 @app.route('/static/<filename>')
@@ -49,9 +79,14 @@ def save(page_id, post_id=None):
         content = request.forms.get('content')
         print("Content: ", content.replace('\r\n', ''))
     else:
+        # it's a new blog page
+        # create new entry in site db file's 'pages' table 
+        # create a new table in site db file
+        
         print("Page id: ", page_id)
         title = request.forms.get('title')
-        print("Title: ", title)
-
+        page_name = slugify(title)
+        print("Title: ", slugify(page_name))
+        # db.table('table_name')
 
 run(app, host=HOST, port=PORT, debug=True, reloader=True)
